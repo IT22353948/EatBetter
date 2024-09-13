59438d4cce7c533d6154789ea131d941ecb819a6
Gayashaan Krishnamoorthy <gayashaan49@gmail.com>
2024-09-13 13:03:30 +0530

    Update navigation_menu.dart

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -23,50 +23,66 @@ class _NavigationMenuState extends State<NavigationMenu> {
     SavedRecipes(),
   ];
 
+  Future<bool> _onPop() async {
+    if (_currentIndex != 0) {
+      // If not on the Menu (index 0), navigate to Menu when back button is pressed
+      setState(() {
+        _currentIndex = 0;
+      });
+      return false; // Prevent default back button behavior
+    } else {
+      return true; // Allow the app to exit if already on Menu
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // IndexedStack keeps the state of pages
-      body: IndexedStack(
-        index: _currentIndex,
-        children: _pages,
-      ),
-      bottomNavigationBar: SizedBox(
-        height: 80,
-        child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          currentIndex: _currentIndex,
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          selectedItemColor: Color.fromARGB(248, 246, 106, 46),
-          unselectedItemColor: const Color.fromARGB(255, 0, 0, 0),
-          showUnselectedLabels: true,
-          iconSize: 25,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.menu_book_rounded),
-              label: 'Menu',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.local_pizza_rounded),
-              label: 'Recipe',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.analytics_outlined),
-              label: 'Analytics',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.location_on_outlined),
-              label: 'Location',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.bookmark),
-              label: 'Saved',
-            ),
-          ],
+    return WillPopScope(
+      //this is depricated we have to use PopScope
+      onWillPop: _onPop,
+      child: Scaffold(
+        // IndexedStack keeps the state of pages
+        body: IndexedStack(
+          index: _currentIndex,
+          children: _pages,
+        ),
+        bottomNavigationBar: SizedBox(
+          height: 80,
+          child: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            currentIndex: _currentIndex,
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            selectedItemColor: Color.fromARGB(248, 246, 106, 46),
+            unselectedItemColor: const Color.fromARGB(255, 0, 0, 0),
+            showUnselectedLabels: true,
+            iconSize: 25,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.menu_book_rounded),
+                label: 'Menu',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.local_pizza_rounded),
+                label: 'Recipe',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.analytics_outlined),
+                label: 'Analytics',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.location_on_outlined),
+                label: 'Location',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.bookmark),
+                label: 'Saved',
+              ),
+            ],
+          ),
         ),
       ),
     );

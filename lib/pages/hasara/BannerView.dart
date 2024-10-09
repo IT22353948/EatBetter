2bfa71c41f6e31f768e76946f4dcde0174d0df3b
Hasara Samarawickrama <Hasaragr1920@gmail.com>
2024-10-09 15:26:16 +0530

    commit

--- lib/pages/hasara/BannerView.dart
+++ lib/pages/hasara/BannerView.dart
@@ -5,25 +5,42 @@ class BannerView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            "Welcome to Restaurant Finder!",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed('/location_view'); // Navigate to MapView
-            },
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color(0xFFF86A2E),
+    // Calculate the height as 25% of the screen height
+    final screenHeight = MediaQuery.of(context).size.height;
+    final containerHeight = screenHeight * 0.25; // 25% of the screen height
+
+    return Container(
+      height: containerHeight, // Set the height of the container
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color.fromARGB(126, 248, 100, 37), // Orange
+            Colors.white, // White
+          ],
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Welcome to Restaurant Finder!",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            child: const Text("Show Map"),
-          ),
-        ],
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/location_view'); // Navigate to MapView
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFFF86A2E),
+              ),
+              child: const Text("Show Map"),
+            ),
+          ],
+        ),
       ),
     );
   }
